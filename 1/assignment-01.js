80ee018e464372a52d33fb30d0cfb45bb59d9600
Anthony <sean.a.protho1@gmail.com>
2020-11-09 23:59:36 -0800

    playing with strings

--- 1/assignment-01.js
+++ 1/assignment-01.js
@@ -1,10 +1,5 @@
-
 // A humble String
 const str = 'hello world'
-
-// Strings have a lot in common with Arrays
-console.log(str[1])
-console.log(str.length)
 
 // Write a function that uppercases the first character of string
 function upperFirst(str) {
@@ -14,40 +9,53 @@ function upperFirst(str) {
 
 // Write a function that uppercases the first letter of each word in a string
 function upperFirstWord(str) {
-  let temp_str = str.split("")
-  let first_world = []
-  let n = ""
-  for (letter in temp_str){
-    if (letter != ' '){
-      n = temp_str[letter].toUpperCase()
-      first_world.push(n)
-    } else {
-      n = temp_str[letter]
-      first_world.push(n)
-    }
+  let temp_str = str.split(" ")
+  let new_string = []
+  for(i in temp_str){
+    word = upperFirst(temp_str[i])
+    new_string.push(word)
   }
-  temp_str = first_world.join("")
-  return temp_str
+  return new_string.join(" ")
 }
 
 // Write a function that uppercases every other letter
+function upperEveryOther(str) {
+  let count = 1
+  let temp_str = str.split("")
+  let new_string = []
+  while (count < str.length + 1){
+    if (count % 2 == 0){
+      new_string.push(temp_str[count-1])
+    } else {
+      let upper = temp_str[count-1].toUpperCase()
+      new_string.push(upper)
+    }
+    count ++
+  }
+  return new_string.join("")
+}
 
 // Write a function that removes all whitespace from a given string
+function removeSpace(str) {
+  let temp_str = str.split("")
+  let new_string = []
+  for (l in str) {
+    if (temp_str[l] != " "){
+      new_string.push(temp_str[l])
+    }
+  }
+  return new_string.join("")
+}
 
 // Write a function that removes only the extra whitespace from a given string (example: “ a        b ” → “a b”)
+function removeExtraSpace(str) {
+  let new_string = str.replace(/\s+/g,' ').trim();
+  return new_string //.join("")
+}
 
-console.log(upperFirst)
-console.log('-----------')
-console.log(upperFirst(str))
-console.log(upperFirst('abcdefg'))
-
-console.log(upperFirstWord)
-console.log('-----------')
-console.log(upperFirstWord(str))
-console.log(upperFirstWord('abcdefg'))
-
-
-
+// Strings have a lot in common with Arrays
+console.log(str[1])
+console.log(str.length)
 
 // Transform a string to uppercase
 console.log(str.toUpperCase())
@@ -60,17 +68,44 @@ console.log(str.replace('hello', 'foo'))
 // replace 
 console.log(str.replace(' ', '-'))
 // replaceAll works in the browser not in node! 
-console.log(str.replaceAll('l', '1'))
+// console.log(str.replaceAll('l', '1'))
 
 // Strings and arrays don't have all the same methods
 // str.push('!') Error push not a function
+console.log('-----------')
+
+console.log(upperFirst)
+console.log('-----------')
+console.log(upperFirst(str))
+console.log(upperFirst('abcdefg'))
+
+console.log(upperFirstWord)
+console.log('-----------')
+console.log(upperFirstWord(str))
+console.log(upperFirstWord('abcdefg'))
+
+console.log(upperEveryOther)
+console.log('-----------')
+console.log(upperEveryOther(str))
+console.log(upperEveryOther('abcdefg'))
+
+console.log(removeSpace)
+console.log('-----------')
+console.log(removeSpace(str))
+console.log(removeSpace('a b c d e f g'))
+
+console.log(removeExtraSpace)
+console.log('-----------')
+console.log(removeExtraSpace(str))
+console.log(removeExtraSpace('a   b  c  d  e f      g'))
 
 String.prototype.first = function() {
   return this[0]
 }
 
 // All strings now have a new method .first()
-
+console.log("[Method: String.first]")
+console.log('-----------')
 console.log(str.first())
 
 // Write a String.last method
@@ -78,6 +113,8 @@ String.prototype.last = function() {
   return this[this.length - 1]
 }
 
+console.log("[Method: String.last]")
+console.log('-----------')
 console.log(str.last())
 
 // Write a String.AllCaps method
@@ -85,4 +122,6 @@ String.prototype.AllCaps = function() {
   return this.toUpperCase()
 }
 
+console.log("[Method: String.AllCaps]")
+console.log('-----------')
 console.log(str.AllCaps())
